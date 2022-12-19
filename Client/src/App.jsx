a40c8a413959665d80c64e35a10040be8b87ed13
Pradip8928 <deepakpal8530@gmail.com>
2022-12-19 15:33:43 +0530

    demo

---
+++ Client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Demo from "./Components/pages/Demo"
+
+function App() {
+ 
+
+  return (
+    <>
+  <Demo/>
+    </>
+  )
+}
+
+export default App
---
+++ Client/src/Components/pages/Demo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import demoCss from "../css/Demo.module.css"
+
+const Demo = () => {
+  return (
+    <div>
+      <h2 className={demoCss.heading} >Giftana here</h2>
+    </div>
+  )
+}
+
+export default Demo
