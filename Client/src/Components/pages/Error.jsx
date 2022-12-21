d528c3ae747a82e1d865b81442145163f8874b6e
Pradip8928 <deepakpal8530@gmail.com>
2022-12-21 22:05:44 +0530

    registeration logic

---
+++ Client/src/Components/pages/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Error = ({  children}) => {
+  return (
+    <div>
+      <div className="alert alert-success text-center" role="alert">
+  <strong>{children}</strong>
+</div>
+    </div>
+  )
+}
+
+export default Error
--- Server/routes/authRoutes/adminRoutes.js
+++ Server/routes/authRoutes/adminRoutes.js
@@ -3,16 +3,17 @@ const router = express.Router();
 const requireLogin = require("../../middleware/requiredLogin/requireLogin");
 
 const {
-  authorizeRoles,
+    authorizeRoles,
 } = require("../../middleware/superAdminAuthenticaton/superAdminAuth");
 
 const {
-  registerAdmin,
-  authAdmin,
-  logoutAdmin,
+    registerAdmin,
+    authAdmin,
+    logoutAdmin,
 } = require("../../controller/authController/adminController");
 
 router.route("/register").post(authorizeRoles("superadmin"), registerAdmin);
+// router.route("/register").post(registerAdmin);
 router.route("/login").post(authorizeRoles("superadmin"), authAdmin);
 router.route("/logout").post(authorizeRoles("superadmin"), logoutAdmin);
 
