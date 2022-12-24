75a758726f3b83b278f6c72e43765bf03bfda456
tiwaririjul <tiwaririjul7@gmail.com>
2022-12-24 16:19:20 +0530

    authentication of admin and superadmin backend

--- Client/src/Components/pages/Register.jsx
+++ Client/src/Components/pages/Register.jsx
@@ -2,26 +2,61 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Loading from "./Loading";
 import Error from "./Error";
+// import Loading from "./Loading";
+import SuccessMessage from "./Success";
 
 const Register = () => {
   const [message, setMessage] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
-  // --
+  const [admin, setAdmin] = useState({
+    userName: "",
+    password: "",
+  });
+
+  let name, value;
+  const handleInputs = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setAdmin({ ...admin, [name]: value });
+  };
+
+  const postData = async (e) => {
+    console.log(admin.userName, admin.password);
+
+    e.preventDefault();
+
+    const { userName, password } = admin;
+
+    const res = await fetch("http://localhost:3000/api/admin/login", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        adminName: userName,
+        password: password,
+      }),
+    });
+    const data = await res.json();
+  };
 
   return (
     <>
-      <form>
+      <form method="POST">
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">
             Email address
           </label>
           <input
-            type="email"
+            type="text"
+            name="userName"
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            value={admin.userName}
+            onChange={handleInputs}
           />
           <div id="emailHelp" className="form-text">
             We'll never share your email with anyone else.
@@ -33,8 +68,10 @@ const Register = () => {
           </label>
           <input
             type="password"
+            name="password"
             className="form-control"
             id="exampleInputPassword1"
+            onChange={handleInputs}
           />
         </div>
         <div className="mb-3 form-check">
@@ -47,7 +84,7 @@ const Register = () => {
             Check me out
           </label>
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" className="btn btn-primary" onClick={postData}>
           Submit
         </button>
       </form>
