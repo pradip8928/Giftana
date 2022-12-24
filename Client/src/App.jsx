dd711345c65e484483a19fc238064408868bb1bf
sumit <jagtapsumit668@gmail.com>
2022-12-24 16:29:29 +0530

    Login Page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,20 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import Demo from "./Components/pages/Demo"
 import Navbar from "./Components/pages/Navbar.jsx";
 import Registraion_Form from './Components/pages/Registraion_Form'
 import '../src/Components/css/Registraion_Form.css';
 import '../src/Components/css/Navbar.css';
+import '../src/Components/css/Login_page.css';
+
+import Login_page from './Components/pages/Login_page';
+
 
 
 function App() {
  
   return (
     <div className="App">
-      <Navbar/>
-      <Registraion_Form/>
-      <Demo/>
+    
+    <Navbar/>
+    <Login_page/>
     
     </div>
   );
--- Client/src/Components/pages/Navbar.jsx
+++ Client/src/Components/pages/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 export default function Navbar() {
- 
-  
+
+
   return (
     <nav class="navbar navbar-expand-lg bg-light">
       <div class="container-fluid">
@@ -22,21 +22,47 @@ export default function Navbar() {
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">
-              Dashboard
-            </a>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Dashboard</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Catolog
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Categories</a></li>
+                <li><a class="dropdown-item" href="#">Manage products</a></li>
+
+                <li><a class="dropdown-item" href="#">Product reviews</a></li>
+                <li><a class="dropdown-item" href="#">Manufactures</a></li>
+                <li><a class="dropdown-item" href="#">Product tags</a></li>
+                <li><a class="dropdown-item" href="#">Low stock report</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <div className="catolog-attribute"><h4>Attributes</h4></div>
+                <li><a class="dropdown-item" href="#">Product Attributes</a></li>
+                <li><a class="dropdown-item" href="#">Specification Attributes</a></li>
+                <li><a class="dropdown-item" href="#">Checkout Attributes</a></li>
+
+
+
+              </ul>
+            </li>
+
             <a class="nav-link" href="#">
-              Catolog
+              Sales
             </a>
-            <a class="nav-link" href="#">
-           Sales
-            </a>
+          
             <a class="nav-link disabled">Customers</a>
-            <a class="nav-link disabled">Promotions</a>
-            <a class="nav-link disabled">CMS</a>
-            <a class="nav-link disabled">Contiguration</a>
-            <a class="nav-link disabled">System</a>
+             <li><a class="nav-link disabled" href="#">Promotions</a></li>
+             <li><a class="nav-link disabled" href="#">CMS</a></li>
+             <li><a class="nav-link disabled" href="#">Contiguration</a></li>
+             <li><a class="nav-link disabled" href="#">System</a></li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Plug</a>
+            </li>
+            
           </div>
+
         </div>
       </div>
     </nav>
