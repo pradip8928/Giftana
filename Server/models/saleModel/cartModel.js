5cea2be0924e67e98716366d552c40750828567e
Pradip8928 <deepakpal8530@gmail.com>
2023-02-07 15:51:19 +0530

    removing proxy

--- Server/models/saleModel/cartModel.js
+++ Server/models/saleModel/cartModel.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-// const cartSchema = new mongoose.Schema({
-//     user: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin', required: true },
-//     cartItems: [{
-//         product: { type: mongoose.Schema.Types.ObjectId, ref: 'ManageProducts', required: true },
-//         quantity: { type: Number, default: 1 },
-//         //price: { type: Number, required: true }
-//     }]
-// }, { timestamps: true });
+
 
 const CartItemSchema = new Schema({
     product: {
